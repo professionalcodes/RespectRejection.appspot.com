7e02f9271c97be89d9ab6345cd36766fe6615d05
professionalcodes <dylan.dannenhauer@protonmail.com>
2017-01-01 15:55:02 +0800

    added bash functions to move the node_modules package in and out of directories

--- utils.sh
+++ utils.sh
@@ -1,8 +1,27 @@
-function mv_node_modules {
-	cp node_modules/ -r
+function mv_node_modules_in {
+	directory=$1
+
+	if [ -d "${directory}/node_modules" ]; then
+		echo "[-] node modules exists at path ${directory}/node_modules"
+	else
+		echo "[+] Moving ~/Desktop/temp/node_modules to ${directory}/"
+		mv ~/Desktop/temp/node_modules/ $directory/
+	fi
+}
+
+function mv_node_modules_out {
+	directory=$1
+
+	if [ ! -d "${directory}/node_modules" ]; then
+		echo "[-] node_modules does not exist in Directory ${directory}"
+	else
+		echo "[+] Moving ${directory}/node_modules to ~/Desktop/temp/"
+		mv $directory/node_modules/ ~/Desktop/temp/
+	fi
 }
 
 function show_git_info {
 	git_user_email=`git config user.email`
 	git_username=`git config user.name`
+	echo "${git_user_email}:${git_username}"
 }
